3e3c0f7161ca3cab24d9d81ac9859d2bdcab834d
crccheck <c@crccheck.com>
2013-05-21 01:51:13 +0000

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='project_runpy',
+    version='0.1-dev.0',
+    author='Chris Chang',
+    author_email='c@crccheck.com',
+    url='https://github.com/crccheck/project_runpy',
+    packages=['heidi', 'tim', ],
+    license='Apache License, Version 2.0',
+    description='Helper utilities for Python projects',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
