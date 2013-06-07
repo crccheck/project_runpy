db44f28d9839c1cb049ce28f9aa5e82758185c99
crccheck <c@crccheck.com>
2013-06-07 02:00:20 +0000

    bump to version 0.1.1
    bugfixes:
    
    * fixes typos in readme
    * fixes bad import style

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='project_runpy',
-    version='0.1.0',
+    version='0.1.1',
     author='Chris Chang',
     author_email='c@crccheck.com',
     url='https://github.com/crccheck/project_runpy',
